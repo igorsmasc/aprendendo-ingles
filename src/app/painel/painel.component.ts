5dbc731bb1a363bd053bfb81864ff880829e9de6
Igor Santos Mascarenhas <Igorsmascarenhas@outlook.com>
2019-05-09 16:55:47 -0300

    Controlando o progresso das respostas

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -17,6 +17,8 @@ export class PainelComponent implements OnInit {
   public rodada:number = 0
   public rodadaFrase: Frase
 
+  public progresso: number = 0
+
   constructor() { 
     this.rodadaFrase = this.frases[this.rodada]
     console.log(this.rodadaFrase) 
@@ -38,6 +40,9 @@ export class PainelComponent implements OnInit {
       
       //Trocar pergunta da rodada
        this.rodada++
+
+      //Progresso
+      this.progresso = this.progresso + (100 / this.frases.length)
 
       //Atualiza o objeto rodadaFrse
        this.rodadaFrase = this.frases[this.rodada]
